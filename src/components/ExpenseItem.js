f3d9a683bc94cd9bcf0d60869d322c21a46f5a90
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-02-18 11:52:46 +0100

    Creating and using the first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>10/02/2023</div>
+      <div>
+        <h2>Subscription renew</h2>
+        <div>250€</div>
+      </div>
+    </div>
+  )
+}
