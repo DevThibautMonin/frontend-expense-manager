2d167de0309528beb6d0e795280ff022cc58b8a8
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-02-18 14:19:35 +0100

    Using hard coded props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const expenses = [
+    { date: new Date(2020, 8, 20), title: "First", amount: 234 },
+    { date: new Date(2019, 4, 24), title: "Second", amount: 3291 },
+    { date: new Date(2017, 2, 25), title: "Third", amount: 98 }
+  ]
+
   return (
-    <ExpenseItem />
+    <div>
+      <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
+      <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount} />
+      <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount} />
+    </div>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css"
 
-export default function ExpenseItem() {
-
-  const expenseDate = new Date(2020, 2, 24)
-  const expenseTitle = "Azure Subscription"
-  const expenseAmount = 500
+export default function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}€</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}€</div>
       </div>
     </div>
   )
