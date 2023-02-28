de0928e91e7470b2da2e0cf6b998782832093f46
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-02-28 18:22:34 +0100

    Using data with Id's

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const INITIAL_EXPENSES = [
-  { date: new Date(2020, 8, 20), title: "First", amount: 234 },
-  { date: new Date(2019, 4, 24), title: "Second", amount: 3291 },
-  { date: new Date(2017, 2, 25), title: "Third", amount: 98 },
-  { date: new Date(2021, 7, 29), title: "Fourth", amount: 9321 },
-  { date: new Date(2021, 8, 5), title: "Fifth", amount: 182 },
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
 ]
 
 const App = () => {
