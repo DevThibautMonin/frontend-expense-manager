b6525ac19cd9ce375dc9882577b3982c15c33808
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-13 20:51:27 +0100

    Update: passing the id to ExpenseItem to delete it

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -13,6 +13,7 @@ const ExpensesList = (props) => {
         props.items.map((expense) => (
           <ExpenseItem
             key={expense.id}
+            id={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
