2b0d1ef13234ef698840edce1dc6978ccf4dfa43
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-13 20:52:27 +0100

    Update: using fontawesome and adding a trash to delete an expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { deleteExpense } from "../../services/expense.service"
 
 const ExpenseItem = (props) => {
+
+  const deleteExpenseHandler = () => {
+    deleteExpense(props.id)
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
+        <FontAwesomeIcon icon={faTrash} onClick={deleteExpenseHandler}/>
       </div>
     </Card>
   )
