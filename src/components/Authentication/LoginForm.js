a58c4b3a9ee800ce17577f2bd12c4ee16ed05bb6
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-17 12:21:39 +0100

    update: login style + loading data after response

--- src/components/Authentication/LoginForm.js
+++ src/components/Authentication/LoginForm.js
@@ -1,7 +1,9 @@
 import { useRef, useState } from "react"
 import { useNavigate } from "react-router"
+import { Link } from "react-router-dom"
 import { login } from "../../services/authentication.service"
 import styles from './LoginForm.module.css'
+import Button from '../UI/Button'
 
 const LoginForm = () => {
   const navigate = useNavigate()
@@ -17,10 +19,12 @@ const LoginForm = () => {
       return
     }
 
-    login(emailRef.current.value, passwordRef.current.value)
-    emailRef.current.value = ''
-    passwordRef.current.value = ''
-    navigate('/expenses')
+    const request = login(emailRef.current.value, passwordRef.current.value)
+    request.then(() => {
+      emailRef.current.value = ''
+      passwordRef.current.value = ''
+      navigate('/expenses')
+    })
 
   }
 
@@ -30,7 +34,8 @@ const LoginForm = () => {
       <input className={error ? styles.invalid : ''} id="email" type="text" placeholder="john.doe@email.com" ref={emailRef} />
       <label htmlFor="password">Password</label>
       <input className={error ? styles.invalid : ''} id="password" type="password" ref={passwordRef} />
-      <button type="submit">Login</button>
+      <p>Don't have an account ? <Link to='/register'>Register</Link></p>
+      <Button type="submit">Login</Button>
     </form>
   )
 }
