be073727eb39a5973bf394e8a413f1f6e6774bf2
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-17 14:50:30 +0100

    update: setting error message and error login component when there is an error

--- src/components/Authentication/LoginForm.js
+++ src/components/Authentication/LoginForm.js
@@ -4,19 +4,23 @@ import { Link } from "react-router-dom"
 import { login } from "../../services/authentication.service"
 import styles from './LoginForm.module.css'
 import Button from '../UI/Button'
+import LoginError from "./LoginError"
 
 const LoginForm = () => {
   const navigate = useNavigate()
-  const [error, setError] = useState(false)
   const emailRef = useRef()
   const passwordRef = useRef()
+  const [isLoginValid, setIsLoginValid] = useState(true)
+  const [error, setError] = useState()
 
   const submitLoginHandler = (event) => {
     event.preventDefault()
 
-    if (!(emailRef.current.value || emailRef.current.value)) {
-      setError(true)
-      return
+    if (emailRef.current.value === '' || emailRef.current.value === '') {
+      setIsLoginValid(false)
+      setError({
+        message: "Email and password can't be empty values."
+      })
     }
 
     const request = login(emailRef.current.value, passwordRef.current.value)
@@ -31,9 +35,10 @@ const LoginForm = () => {
   return (
     <form onSubmit={submitLoginHandler} className={styles.form}>
       <label htmlFor="email">Email</label>
-      <input className={error ? styles.invalid : ''} id="email" type="text" placeholder="john.doe@email.com" ref={emailRef} />
+      <input className={!isLoginValid ? styles.invalid : ''} id="email" type="text" placeholder="john.doe@email.com" ref={emailRef} />
       <label htmlFor="password">Password</label>
-      <input className={error ? styles.invalid : ''} id="password" type="password" ref={passwordRef} />
+      <input className={!isLoginValid ? styles.invalid : ''} id="password" type="password" ref={passwordRef} />
+      {!isLoginValid && <LoginError errorMessage={error.message} />}
       <p>Don't have an account ? <Link to='/register'>Register</Link></p>
       <Button type="submit">Login</Button>
     </form>
