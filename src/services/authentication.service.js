4652b45a877fd649f7ce50630b2fe6df6eb92c43
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-20 16:13:04 +0100

    authentication service: register method

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -15,3 +15,16 @@ export const login = async (email, password) => {
   }
 
 }
+
+export const register = async (username, email, password) => {
+  try {
+    const response = await axios.post(`${url}/authentication/register`, {
+      username: username,
+      email: email,
+      password: password
+    })
+    return response
+  } catch (error) {
+    return error.response.status
+  }
+}
