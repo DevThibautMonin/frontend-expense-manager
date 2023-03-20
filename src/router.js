b65de67543ad5bd5fba3f6dda52e9efb5a2a8049
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-20 15:47:33 +0100

    add: register route

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 import LoginForm from '../src/components/Authentication/LoginForm'
+import RegisterForm from './components/Authentication/RegisterForm'
 import ExpensesPage from './pages/ExpensesPage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <LoginForm /> },
+  { path: '/register', element: <RegisterForm /> },
   { path: '/expenses', element: <ExpensesPage /> }
 ])
