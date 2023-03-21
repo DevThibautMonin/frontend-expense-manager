d1509bbdd72142909b92bd7c2c4a9c58601940ff
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-21 18:35:47 +0100

    Updating YearFilter (clean code)

--- src/components/ExpensesFilters/YearFilter.js
+++ src/components/ExpensesFilters/YearFilter.js
@@ -5,19 +5,16 @@ const YearFilter = (props) => {
     props.onYearChange(selectedYear)
   }
 
+  const years = ['2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025'].reverse()
+
   return (
     <>
       <select value={props.year} onChange={yearFilterHandler}>
-        <option value='2026'>2026</option>
-        <option value='2025'>2025</option>
-        <option value='2024'>2024</option>
-        <option value='2023'>2023</option>
-        <option value='2022'>2022</option>
-        <option value='2021'>2021</option>
-        <option value='2020'>2020</option>
-        <option value='2019'>2019</option>
-        <option value='2018'>2018</option>
-        <option value='2017'>2017</option>
+        {
+          years.map(y =>
+            <option value={y}>{y}</option>
+          )
+        }
       </select>
     </>
   )
