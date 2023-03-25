86ec2f36273f099d5271c15f3feef7bc89e684b5
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-25 21:48:40 +0100

    Removing useless code, we are not in mvc

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -2,20 +2,6 @@ import axios from "axios"
 
 const url = 'http://localhost:4500'
 
-export const login = async (email, password) => {
-  try {
-    const response = await axios.post(`${url}/authentication/login`, {
-      email: email,
-      password: password
-    })
-    localStorage.setItem('token', response.data.token)
-    return response.status
-  } catch (error) {
-    return error.response.status
-  }
-
-}
-
 export const register = async (username, email, password) => {
   try {
     const response = await axios.post(`${url}/authentication/register`, {
