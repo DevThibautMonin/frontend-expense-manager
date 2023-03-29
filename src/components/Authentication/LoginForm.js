4e91b03c6323902a2802b7b1d590ddf2109604b0
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-29 17:07:29 +0200

    feat(login form validation): improving error feedback for users

--- src/components/Authentication/LoginForm.js
+++ src/components/Authentication/LoginForm.js
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from "react-redux"
 import Loader from '../UI/Loader'
 import { useNavigate } from "react-router"
 import { login } from "../../store/actions/authentication.actions"
-import { uiActions } from "../../store/slices/ui.slice"
 
 const LoginForm = () => {
   const [emailInput, setEmailInput] = useState('')
@@ -41,11 +40,6 @@ const LoginForm = () => {
   const emailChangeHandler = (event) => {
     setEmailInput(event.target.value)
     setIsEmailTouched(true)
-    if (isEmailInputInvalid) {
-      dispatch(uiActions.setError("Please provide a valid email."))
-    } else {
-      dispatch(uiActions.setError(null))
-    }
   }
 
   const emailInputBlurHandler = () => {
@@ -55,11 +49,6 @@ const LoginForm = () => {
   const passwordChangeHandler = (event) => {
     setPasswordInput(event.target.value)
     setIsPasswordTouched(true)
-    if (isPasswordInputInvalid) {
-      dispatch(uiActions.setError("Please provide a valid password."))
-    } else {
-      dispatch(uiActions.setError(null))
-    }
   }
 
   const passwordInputBlurHandler = () => {
@@ -75,8 +64,10 @@ const LoginForm = () => {
     <form onSubmit={submitLoginHandler} className={styles.form}>
       <label htmlFor="email">Email</label>
       <input className={isEmailInputInvalid ? styles.invalid : ''} id="email" type="email" onChange={emailChangeHandler} onBlur={emailInputBlurHandler} placeholder="john.doe@email.com" />
+      {isEmailInputInvalid && <LoginError errorMessage={"Please enter a valid email"} />}
       <label htmlFor="password">Password</label>
       <input className={isPasswordInputInvalid ? styles.invalid : ''} id="password" type="password" onChange={passwordChangeHandler} onBlur={passwordInputBlurHandler} placeholder='********' />
+      {isPasswordInputInvalid && <LoginError errorMessage={"Please enter a valid password"} />}
       {error && <LoginError errorMessage={error} />}
       <p>Don't have an account ? <Link to='/register'>Register</Link></p>
       {!error && isLoading && <Loader />}
