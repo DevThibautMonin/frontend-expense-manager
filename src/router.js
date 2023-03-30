9013163f14b56fabcf1d33859537eb7531a8863e
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-30 21:01:10 +0200

    feat(user-profile): updating router

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import { createBrowserRouter } from 'react-router-dom'
 import LoginForm from '../src/components/Authentication/LoginForm'
 import RegisterForm from './components/Authentication/RegisterForm'
 import ExpensesPage from './pages/ExpensesPage'
+import UserProfilePage from './pages/UserProfilePage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <LoginForm /> },
   { path: '/register', element: <RegisterForm /> },
-  { path: '/expenses', element: <ExpensesPage /> }
+  { path: '/expenses', element: <ExpensesPage /> },
+  { path: '/user-profile', element: <UserProfilePage /> }
 ])
