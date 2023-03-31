6d856f70161a6a293c65ac3776d2c3c27cfd8436
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-31 19:50:27 +0200

    updaing router

--- src/router.js
+++ src/router.js
@@ -2,11 +2,11 @@ import { createBrowserRouter } from 'react-router-dom'
 import LoginForm from '../src/components/Authentication/LoginForm'
 import RegisterForm from './components/Authentication/RegisterForm'
 import ExpensesPage from './pages/ExpensesPage'
-import UserProfilePage from './pages/UserProfilePage'
+import ProfilePage from './pages/ProfilePage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <LoginForm /> },
   { path: '/register', element: <RegisterForm /> },
   { path: '/expenses', element: <ExpensesPage /> },
-  { path: '/user-profile', element: <UserProfilePage /> }
+  { path: '/profile', element: <ProfilePage /> }
 ])
