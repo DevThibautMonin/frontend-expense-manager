ae5bddfa2ca2aecab83bfd2b9018bddc5f291115
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-31 21:09:45 +0200

    Profile added into profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/UI/Navbar"
+import Profile from "../components/Profile/Profile"
 
 const ProfilePage = (props) => {
   return (
     <>
       <Navbar />
+      <Profile />
     </>
   )
 }
