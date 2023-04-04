37373e20dba485b574ca1bc715d8ea79567392c1
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-04-04 20:49:18 +0200

    feat:(profile page): Removing profile page to focus other features and fix problems before introducing new features

--- src/router.js
+++ src/router.js
@@ -2,11 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import LoginForm from '../src/components/Authentication/LoginForm'
 import RegisterForm from './components/Authentication/RegisterForm'
 import ExpensesPage from './pages/ExpensesPage'
-import ProfilePage from './pages/ProfilePage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <LoginForm /> },
   { path: '/register', element: <RegisterForm /> },
   { path: '/expenses', element: <ExpensesPage /> },
-  { path: '/profile', element: <ProfilePage /> }
 ])
