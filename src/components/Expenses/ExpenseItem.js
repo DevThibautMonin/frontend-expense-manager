dc24a3829e12438247e169dc97f8a618e4f31be6
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-04-05 21:12:42 +0200

    Removing edit icon

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch } from "react-redux"
 import { deleteExpense } from "../../store/actions/expense.actions"
 
@@ -20,7 +20,6 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title} ({props.category})</h2>
         <div className="expense-item__price">{props.amount}€</div>
-        <FontAwesomeIcon className="edit-button" icon={faEdit} onClick={deleteExpenseHandler} />
         <FontAwesomeIcon className="delete-button" icon={faTrash} onClick={deleteExpenseHandler} />
       </div>
     </Card>
